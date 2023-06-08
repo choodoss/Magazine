2625fe046d2677345c1688badfb0d2ab3e5dd2ee
Дмитрий Хоменко <dimkakhomenko@gmail.com>
2023-06-08 14:33:58 +0300

    add start files

---
+++ src/js/ERCmagazine.js
@@ -0,0 +1,27 @@
+import content from './textArray';
+
+const lengthInColumn = content.length;
+const quantityFirstColumn = Math.ceil(lengthInColumn / 2);
+const quantitySecondColumn = lengthInColumn - quantityFirstColumn;
+
+const divMain = document.querySelectorAll('.magazine-contents__col');
+
+const firstColumn = [];
+const secondColumn = [];
+
+content.forEach(({ number, name, text }, index) => {
+    const column = index < quantityFirstColumn ? firstColumn : secondColumn;
+    column.push(`<div class="magazine-contents__item">
+    <div class="magazine-contents__number">${number}</div>
+    <div class="magazine-contents__content">
+      <div class="magazine-contents__name">${name}</div>
+      <div class="magazine-contents__text">${text}</div>
+    </div>
+  </div>`);
+});
+
+divMain[0].insertAdjacentHTML("afterbegin", firstColumn.join(""));
+divMain[1].insertAdjacentHTML("afterbegin", secondColumn.join(""));
+
+
+
